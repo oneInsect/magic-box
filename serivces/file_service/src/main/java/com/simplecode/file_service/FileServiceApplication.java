3e479350292e52e4796cad8700a337e424ff8396
Lance <xfeng_insect@foxmail.com>
2020-08-16 15:18:41 +0800

    add color select

---
+++ serivces/file_service/src/main/java/com/simplecode/file_service/FileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simplecode.file_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServiceApplication.class, args);
+    }
+
+}
