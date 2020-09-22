cb3d98e244da37690ea50718689c468b6c22cd6d
Lance <xfeng_insect@foxmail.com>
2020-09-22 23:39:21 +0800

    add mysql dockerfile

--- middleware/mysql/start.sh
+++ middleware/mysql/start.sh
@@ -6,8 +6,13 @@ then
     chown mysql:mysql /var/lib/mysql -R
     systemctl start mysqld.service
     oldpwd=`cat /var/log/mysqld.log | grep password | awk -F ": " '{print $2}'`
-    mysqladmin -uroot -p"${oldpwd}" password admin
+    mysqladmin -uroot -p"${oldpwd}" password 'Changeme_123'
     touch /opt/mysql/initialized
 else
     systemctl start mysqld.service
 fi
+
+while true
+do
+    sleep 1000
+done
