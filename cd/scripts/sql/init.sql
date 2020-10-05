9f89c02249ab5df8642059db703b608e459e5214
lance <739056672@qq.com>
2020-10-05 23:52:51 +0800

    add cd sql

---
+++ cd/scripts/sql/init.sql
@@ -0,0 +1,14 @@
+
+CREATE DATABASE magic_service_mgt;
+USE magic_service_mgt;
+
+CREATE TABLE IF NOT EXISTS `t_images` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `image_name` varchar(50) not null comment 'Image Name',
+  `image_path` varchar(100) not null comment 'Image Path',
+  `is_deleted` tinyint(1) unsigned not null default '0' comment 'logic delete 0: (false) exist, 1: (true) deleted',
+  `created_time` datetime not null comment 'create time',
+  `modified_time` datetime not null comment 'update time',
+  primary key (`id`),
+  unique key `uk_path` (`image_path`)
+)ENGINE = innodb DEFAULT CHARSET = utf8mb4;
