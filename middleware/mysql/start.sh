0dd533eb245d449e13d980ef8f29564c6805c5c2
lance <739056672@qq.com>
2020-10-18 23:04:35 +0800

    add service mgt

--- middleware/mysql/start.sh
+++ middleware/mysql/start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-if [ -f /opt/mysql/initialized ]
+now=`date`
+
+if [[ ! -f /opt/mysql/initialized ]]
 then
     mysqld --initialize
     chown mysql:mysql /var/lib/mysql -R
     systemctl start mysqld.service
     oldpwd=`cat /var/log/mysqld.log | grep password | awk -F ": " '{print $2}'`
     mysqladmin -uroot -p"${oldpwd}" password 'Changeme_123'
-    touch /opt/mysql/initialized
+    echo "${now} complete initialize." >> /opt/mysql/initialized
 else
     systemctl start mysqld.service
+    echo "${now} restart mysql container." >> /opt/mysql/initialized
 fi
 
 while true
